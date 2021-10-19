5aa97fca84db6961cafd62e2d849ca3f5510bcbb
Silas Boyd-Wickizer <silasbw@gmail.com>
2021-10-19 11:23:26 -0700

    Copy from https://github.com/valora-inc/wallet/tree/main/packages/blockchain-api (#1)

---
+++ src/events/Any.ts
@@ -0,0 +1,18 @@
+import { metrics } from '../metrics'
+import { Transaction } from '../transaction/Transaction'
+import { TransactionType } from '../transaction/TransactionType'
+
+export class Any extends TransactionType {
+  matches(transaction: Transaction): boolean {
+    return true
+  }
+
+  getEvent(transaction: Transaction) {
+    metrics.unknownTransaction()
+    throw new Error('Unknown transaction type')
+  }
+
+  isAggregatable(): boolean {
+    return false
+  }
+}
