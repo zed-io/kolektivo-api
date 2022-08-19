193437f6e39906eacecdb6ffa9d562205015f8bd
Matthew Abraham <wasteofintel@gmail.com>
2022-08-19 15:28:02 -0400

    (kw-120) Deploy to mainnet (#5)
    * feat: use coingecko api for tokens
    
    * feat: add coingecko to env
    
    * chore: deploy on specific version number
    
    * chore: update deploy secrets
    
    * fix: update pg password
    
    * feat: add sentry release stage to deploy
    
    * fix: redeploy sql server to na
    
    * chore: point to new prod fb rtdb

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,28 @@ set -euo pipefail
 
 NETWORK=""
 
-while getopts 'n:p:' flag; do
+while getopts 'n:p:k:s:' flag; do
   case "${flag}" in
     n) NETWORK="$OPTARG" ;;
+    k) SENTRY_KEY="$OPTARG" ;;
+    s) SEMVER="$OPTARG" ;;
     *) echo "Unexpected option ${flag}" ;;
   esac
 done
 
 [ -z "$NETWORK" ] && echo "Need to set the NETWORK via the -n flag" && exit 1;
 
+function sentry_release () {
+  # Setup configuration values
+  SENTRY_AUTH_TOKEN=$SENTRY_KEY
+  SENTRY_ORG=zed-labs
+  SENTRY_PROJECT=kolektivo-blockchain-api
+  VERSION=`sentry-cli releases $SEMVER`
+  # Workflow to create releases
+  sentry-cli releases new "$VERSION"
+  sentry-cli releases set-commits "$VERSION" --auto
+  sentry-cli releases finalize "$VERSION"
+}
 PROJECT="kolektivo-backend"
 
 echo "Starting blockchain api deployment."
@@ -24,6 +37,11 @@ echo "Starting blockchain api deployment."
 echo 'Deploying to gcloud'
 gcloud --project ${PROJECT} app deploy -q "app.${NETWORK}.yaml"
 gcloud --project ${PROJECT} app deploy -q cron.yaml
+
+[ -z "$SENTRY_KEY" ] && echo "Sentry key not set, skipping sentry release" && exit 0;
+[ -z "$SEMVER" ] && echo "Version not set, skipping sentry release" && exit 0;
+
+sentry_release
 
 echo 'Hitting service url to trigger update'
 # This seems to be necessary to ensure get App Engine starts the service
