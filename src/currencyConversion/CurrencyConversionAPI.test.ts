5312897000d342258ed69111004720a82e57ec01
Matthew Abraham <14793093+shottah@users.noreply.github.com>
2022-10-12 18:28:34 -0400

    (KW-186) API Reaches rate limit against Coin Gecko (#6)
    * chore: add sqlite3 support for jest
    
    * feat: use firebase usd prices
    
    * fix: unable to convert FIAT to cEUR
    
    Co-authored-by: Matthew Abraham <wasteofintel@gmail.com>

--- src/currencyConversion/CurrencyConversionAPI.test.ts
+++ src/currencyConversion/CurrencyConversionAPI.test.ts
@@ -7,6 +7,7 @@ import OracleJsonAPI from './OracleJsonAPI'
 
 jest.mock('./ExchangeRateAPI')
 jest.mock('./GoldExchangeRateAPI')
+jest.mock('./OracleJsonAPI')
 
 const mockDefaultGetExchangeRate = ExchangeRateAPI.prototype
   .getExchangeRate as jest.Mock
@@ -46,9 +47,9 @@ describe('CurrencyConversionAPI', () => {
       currencyCode: 'cUSD',
       impliedExchangeRates,
     })
-    expect(result).toEqual(new BigNumber(10))
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(0)
+    expect(result).toEqual(new BigNumber(10))
   })
 
   it('should retrieve rate for cUSD/cGLD', async () => {
@@ -56,9 +57,10 @@ describe('CurrencyConversionAPI', () => {
       sourceCurrencyCode: 'cUSD',
       currencyCode: 'cGLD',
     })
-    expect(result).toEqual(new BigNumber(10))
+    
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(1)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(1)
+    expect(result).toEqual(new BigNumber(15))
   })
 
   it('should retrieve rate for cGLD/USD', async () => {
@@ -68,10 +70,10 @@ describe('CurrencyConversionAPI', () => {
       currencyCode: 'USD',
       impliedExchangeRates,
     })
-    expect(result).toEqual(new BigNumber(10))
 
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(0)
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(result).toEqual(new BigNumber(10))
   })
 
   it('should retrieve rate for USD/cGLD', async () => {
@@ -79,9 +81,9 @@ describe('CurrencyConversionAPI', () => {
       sourceCurrencyCode: 'USD',
       currencyCode: 'cGLD',
     })
-    expect(result).toEqual(new BigNumber(10))
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(1)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(1)
+    expect(result).toEqual(new BigNumber(15))
   })
 
   it('should retrieve rate for cGLD/MXN', async () => {
@@ -91,9 +93,9 @@ describe('CurrencyConversionAPI', () => {
       currencyCode: 'MXN',
       impliedExchangeRates,
     })
-    expect(result).toEqual(new BigNumber(200))
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(1)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(0)
+    expect(result).toEqual(new BigNumber(200))
   })
 
   it('should retrieve rate for MXN/cGLD', async () => {
@@ -101,9 +103,9 @@ describe('CurrencyConversionAPI', () => {
       sourceCurrencyCode: 'MXN',
       currencyCode: 'cGLD',
     })
-    expect(result).toEqual(new BigNumber(200))
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(1)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(1)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(1)
+    expect(result).toEqual(new BigNumber(300))
   })
 
   it('should retrieve rate for USD/MXN', async () => {
@@ -111,9 +113,9 @@ describe('CurrencyConversionAPI', () => {
       sourceCurrencyCode: 'USD',
       currencyCode: 'MXN',
     })
-    expect(result).toEqual(new BigNumber(20))
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(1)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(0)
+    expect(result).toEqual(new BigNumber(20))
   })
 
   it('should retrieve rate for MXN/USD', async () => {
@@ -123,7 +125,8 @@ describe('CurrencyConversionAPI', () => {
     })
     expect(result).toEqual(new BigNumber(20))
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(1)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(0)
+    expect(result).toEqual(new BigNumber(20))
   })
 
   it('should retrieve rate for cUSD/MXN', async () => {
@@ -133,7 +136,8 @@ describe('CurrencyConversionAPI', () => {
     })
     expect(result).toEqual(new BigNumber(20))
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(1)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(0)
+    expect(result).toEqual(new BigNumber(20))
   })
 
   it('should retrieve rate for MXN/cUSD', async () => {
@@ -143,7 +147,8 @@ describe('CurrencyConversionAPI', () => {
     })
     expect(result).toEqual(new BigNumber(20))
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(1)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(0)
+    expect(result).toEqual(new BigNumber(20))
   })
 
   it('should return 1 when using the same currency code', async () => {
@@ -151,9 +156,9 @@ describe('CurrencyConversionAPI', () => {
       sourceCurrencyCode: 'ABC',
       currencyCode: 'ABC',
     })
-    expect(result).toEqual(new BigNumber(1))
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledTimes(0)
+    expect(result).toEqual(new BigNumber(1))
   })
 
   it('should retrieve rate for cGLD/EUR', async () => {
@@ -161,9 +166,8 @@ describe('CurrencyConversionAPI', () => {
       sourceCurrencyCode: 'cGLD',
       currencyCode: 'EUR',
     })
-    expect(result).toEqual(new BigNumber(200))
 
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledWith({
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledWith({
       sourceCurrencyCode: 'cGLD',
       currencyCode: 'cUSD',
     })
@@ -171,6 +175,7 @@ describe('CurrencyConversionAPI', () => {
       sourceCurrencyCode: 'USD',
       currencyCode: 'EUR',
     })
+    expect(result).toEqual(new BigNumber(300))
   })
 
   it('should retrieve rate for EUR/cGLD', async () => {
@@ -178,15 +183,76 @@ describe('CurrencyConversionAPI', () => {
       sourceCurrencyCode: 'EUR',
       currencyCode: 'cGLD',
     })
-    expect(result).toEqual(new BigNumber(200))
-
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledWith({
       sourceCurrencyCode: 'EUR',
       currencyCode: 'USD',
     })
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledWith({
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledWith({
       sourceCurrencyCode: 'cUSD',
       currencyCode: 'cGLD',
     })
+    expect(result).toEqual(new BigNumber(300))
+  })
+
+  it('should retrieve rate for cGLD/cEUR', async () => {
+    const result = await currencyConversionAPI.getExchangeRate({
+      sourceCurrencyCode: 'cGLD',
+      currencyCode: 'cEUR'
+    })
+    expect(mockDefaultGetExchangeRate).toHaveBeenCalledWith({
+      sourceCurrencyCode: 'USD',
+      currencyCode: 'EUR',
+    })
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledWith({
+      sourceCurrencyCode: 'cGLD',
+      currencyCode: 'cUSD',
+    })
+    expect(result).toEqual(new BigNumber(300))
+  })
+
+  it('should retrieve rate for cEUR/cGLD', async () => {
+    const result = await currencyConversionAPI.getExchangeRate({
+      sourceCurrencyCode: 'cEUR',
+      currencyCode: 'cGLD'
+    })
+    expect(mockDefaultGetExchangeRate).toHaveBeenCalledWith({
+      sourceCurrencyCode: 'EUR',
+      currencyCode: 'USD',
+    })
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledWith({
+      sourceCurrencyCode: 'cUSD',
+      currencyCode: 'cGLD',
+    })
+    expect(result).toEqual(new BigNumber(300))
+  })
+
+  it('should retrieve rate for TTD/cEUR', async () => {
+    const result = await currencyConversionAPI.getExchangeRate({
+      currencyCode: 'TTD',
+      sourceCurrencyCode: 'cEUR'
+    })
+
+    expect(mockDefaultGetExchangeRate).toHaveBeenCalledWith({
+      currencyCode: 'TTD',
+      sourceCurrencyCode: 'USD',
+    })
+    expect(mockDefaultGetOracleRate).toHaveBeenCalledWith({
+      currencyCode: 'USD',
+      sourceCurrencyCode: 'cEUR'
+    })
+    expect(result).toEqual(new BigNumber(300))
+  })
+
+  it('should retrieve rate for TTD/GBR', async () => {
+    const result = await currencyConversionAPI.getExchangeRate({
+      currencyCode: 'TTD',
+      sourceCurrencyCode: 'GBR'
+    })
+
+    expect(mockDefaultGetExchangeRate).toHaveBeenCalledWith({
+      currencyCode: 'TTD',
+      sourceCurrencyCode: 'GBR',
+    })
+    expect(result).toEqual(new BigNumber(20))
   })
 })
--- src/currencyConversion/OracleJsonAPI.ts
+++ src/currencyConversion/OracleJsonAPI.ts
@@ -1,22 +1,13 @@
 import { RESTDataSource } from 'apollo-datasource-rest'
-import { metrics } from '../metrics'
-import { COINGECKO_API } from '../config'
-import { enumValueForKey, getCurrency, supportedOracleTokens } from './consts'
+import { CELO, CGLD, CUSD, USD } from './consts'
 import BigNumber from 'bignumber.js'
 import { logger } from '../logger'
 import { CurrencyConversionArgs } from '../resolvers'
-import { formatDateStringShort } from '../utils'
-
-interface OracleApiResult {
-  market_data: { current_price: { [currencyCode: string]: number } }
-}
-
-const MIN_TTL = 12 * 3600 // 12 hours
+import tokenInfoCache, { TokenInfo } from '../helpers/TokenInfoCache'
 
 export default class OracleJsonAPI extends RESTDataSource {
   constructor() {
     super()
-    this.baseURL = `${COINGECKO_API}`
   }
 
   async getExchangeRate({
@@ -25,17 +16,26 @@ export default class OracleJsonAPI extends RESTDataSource {
     timestamp,
   }: CurrencyConversionArgs): Promise<BigNumber> {
     try {
+      // Note: Can be any pair in `supportedPairs`
+      const toToken = (sourceCurrencyCode === CGLD ? CELO : sourceCurrencyCode) || CUSD;
+      const fromToken = currencyCode === CGLD ? CELO : currencyCode;
       const pair = `${sourceCurrencyCode}/${currencyCode}`
-      const rates = await this.getOracleRate(
-        sourceCurrencyCode || supportedOracleTokens.cUSD,
-        currencyCode,
-        timestamp,
-      )
-      const payload = Object.assign({}, rates.market_data.current_price)
-      if (!payload[getCurrency(currencyCode)]) {
+      // Note: If converting from cUSD -> X, 
+      // then the rate needs to be inverted
+      let rate;
+      if (toToken === CUSD || toToken === USD) {
+        rate = this.getFirebaseRate(fromToken)
+      } else if (fromToken === CUSD || fromToken == USD) {
+        const temp = await this.getFirebaseRate(toToken);
+        rate = new BigNumber(1).dividedBy(temp)
+      }
+      
+      if (!rate) {
         throw new Error(`Unable to get rate for pair: ${pair}`)
       }
-      return new BigNumber(payload[getCurrency(currencyCode)])
+
+      logger.info(rate.toString())
+      return rate
     } catch (error) {
       logger.error({
         type: 'ERROR_UPDATING_ORACLE_PRICES',
@@ -45,37 +45,21 @@ export default class OracleJsonAPI extends RESTDataSource {
     }
   }
 
-  async getOracleRate(
-    sourceCurrencyCode: string,
-    currencyCode: string,
-    timestamp: number = Date.now(),
-  ): Promise<OracleApiResult> {
-    const t0 = performance.now()
-    const date = new Date(Date.now())
-    const path = `/coins/${enumValueForKey(
-      supportedOracleTokens,
-      sourceCurrencyCode,
-    )}/history`
-    const params = {
-      date: formatDateStringShort(new Date(timestamp)),
-    }
-    const result = await this.get<OracleApiResult>(path, params, {
-      cacheOptions: { ttl: this.getCacheTtl(date) },
-    })
-    if (Object.is(result, {})) {
-      throw new Error(`Invalid response from oracle: ${JSON.stringify(result)}`)
-    }
-    const t1 = performance.now()
-    metrics.setQueryOracleRateDuration(t1 - t0)
-    return result
-  }
-
-  private getCacheTtl(date: Date) {
-    if (Date.now() - date.getTime() >= 24 * 3600 * 1000) {
-      // Cache indefinitely if requesting a date prior to the last 24 hours
-      return Number.MAX_SAFE_INTEGER
-    } else {
-      return MIN_TTL
-    }
+  /**
+   * This function returns the currently stored USD price of a 
+   * given crypto asset by address.
+   * 
+   * e.g. cGLD -> USD = 13.034094
+   * @param tokenAddress Address of Asset to return USD Price
+   * @returns {Promise<BigNumber>}
+   */
+  async getFirebaseRate(
+    currency: string,
+  ): Promise<BigNumber> {
+    logger.info(currency)
+    const tokenInfo: TokenInfo | undefined = tokenInfoCache.tokenInfoBySymbol(currency)
+    const usdPrice: BigNumber = new BigNumber(tokenInfo?.usdPrice ?? 0)
+    logger.info(tokenInfo)
+    return usdPrice;
   }
 }
