36301cfa02421d7ba387b67a9ddd157a41ad1591
Jean Regisser <jean.regisser@gmail.com>
2023-02-24 17:35:06 +0100

    fix: set min liquidity to 10k to get cREAL price back (#234)
    ### Description
    
    cREAL liquidity dropped bellow $20k on 2023-02-16 which caused the cREAL
    price to become unavailable in Valora.
    
    This PR decreases the min liquidity to $10k to fix this.
    
    See Slack
    [thread](https://valora-app.slack.com/archives/C029Z1QMD7B/p1677235014387249?thread_ts=1677089358.452749&cid=C029Z1QMD7B).
    
    ### Tested
    
    cREAL price becomes available again.
    
    ### Backwards compatibility
    
    Yes
    
    ### Related issues
    
    N/A

--- src/index.ts
+++ src/index.ts
@@ -123,7 +123,14 @@ async function main() {
   })
 
   const exchangeRateConfig = exchangesConfigs[args['exchanges-network-config']]
-  const exchangeRateManager = createNewManager(exchangeRateConfig)
+  const exchangeRateManager = createNewManager({
+    ...exchangeRateConfig,
+    ubeswap: {
+      ...exchangeRateConfig.ubeswap,
+      // cREAL liquidity dropped below 20k on 2023-02-16, so we're lowering the threshold for now
+      minLiquidity: 10_000,
+    },
+  })
 
   knownAddressesCache.startListening()
   tokenInfoCache.startListening()
