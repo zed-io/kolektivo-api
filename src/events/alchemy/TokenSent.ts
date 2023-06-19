6d198c2dee371cefbcaffc32329e423bd3062c79
Charlie Andrews-Jubelt <cajubelt@users.noreply.github.com>
2023-06-19 12:21:11 -0700

    feat(alchemy): TokenSent.getEvent (#271)
    ### Description
    
    implemented TokenSent.getEvent
    
    NOTE: punts on handling fees, which are paid for in Eth and probably
    require a breaking change to the API of the service. I am punting on
    this problem since it was not anticipated as part of this issue and
    increases scope significantly. See issue
    [here](https://linear.app/valora/issue/ACT-840/display-fees-correctly-when-paid-in-native-token)
    
    ### Tested
    
    new unit tests
    
    ### Backwards compatibility
    
    na
    
    ### Related issues
    
    cc
    https://linear.app/valora/issue/ACT-792/serialize-classified-alchemy-transactions

--- src/events/alchemy/TokenSent.ts
+++ src/events/alchemy/TokenSent.ts
@@ -1,6 +1,7 @@
-import { TokenTransferV2 } from '../../types'
+import { TokenTransactionTypeV2, TokenTransferV2 } from '../../types'
 import { TransactionType } from '../../transaction/TransactionType'
 import { AlchemyTransaction } from '../../transaction/alchemy/AlchemyTransaction'
+import knownAddressesCache from '../../helpers/KnownAddressesCache'
 
 export class TokenSent extends TransactionType<AlchemyTransaction> {
   matches(transaction: AlchemyTransaction): boolean {
@@ -10,7 +11,43 @@ export class TokenSent extends TransactionType<AlchemyTransaction> {
     )
   }
 
-  async getEvent(_transaction: AlchemyTransaction): Promise<TokenTransferV2> {
-    throw new Error('not implemented')
+  async getEvent(transaction: AlchemyTransaction): Promise<TokenTransferV2> {
+    const [
+      {
+        metadata: { blockTimestamp },
+        blockNum: block,
+        hash: transactionHash,
+        value,
+        rawContract: { address: tokenAddress },
+        to: toAddress,
+      },
+    ] = transaction.getErc20TransfersFrom()
+    if (!value || !tokenAddress || !toAddress) {
+      throw new Error(
+        `getEvent called on TokenSent with missing required value. transactionHash: ${transactionHash}`,
+      )
+    }
+    // get title, image from knownAddressesCache
+    const { name: title, imageUrl: image } =
+      knownAddressesCache.getDisplayInfoFor(toAddress)
+    const timestamp = new Date(blockTimestamp).getTime()
+    return {
+      type: TokenTransactionTypeV2.SENT,
+      timestamp,
+      block,
+      transactionHash,
+      address: toAddress,
+      account: toAddress,
+      amount: {
+        value, // according to Alchemy docs this is already in units of Eth, not Wei. https://docs.alchemy.com/reference/sdk-getassettransfers#assettransfersresponse-response-object-parameters
+        tokenAddress,
+        timestamp,
+      },
+      metadata: {
+        title,
+        image,
+      },
+      fees: [], // TODO add fees once wallet can handle fees paid in native currency https://linear.app/valora/issue/ACT-840/display-fees-correctly-when-paid-in-native-token
+    }
   }
 }
--- test/events/alchemy/TokenSent.test.ts
+++ test/events/alchemy/TokenSent.test.ts
@@ -8,6 +8,7 @@ import {
   mockTokenSwapTx,
   mockTxReceipt,
 } from '../../mock-data/alchemy'
+import knownAddressesCache from '../../../src/helpers/KnownAddressesCache'
 
 describe('TokenSent', () => {
   const tokenSent = new TokenSent({ userAddress: 'some-address' })
@@ -33,6 +34,56 @@ describe('TokenSent', () => {
 
     it('returns false for tx with both erc 20 transfers from and to', () => {
       expect(tokenSent.matches(mockTokenSwapTx)).toEqual(false)
+    })
+  })
+
+  describe('getEvent', () => {
+    it.each([
+      { value: null },
+      {
+        rawContract: {
+          address: null,
+          value: '0x123',
+          decimal: '0x18',
+        },
+      },
+    ])('throws when required field is missing', async (partialTransferFrom) => {
+      await expect(() =>
+        tokenSent.getEvent(
+          new AlchemyTransaction({
+            transfersFrom: [],
+            transfersTo: [{ ...mockErc20Transfer, ...partialTransferFrom }],
+            txReceipt: mockTxReceipt,
+          }),
+        ),
+      ).rejects.toThrowError()
+    })
+    it('correctly maps AlchemyTransaction to TokenTransferV2', async () => {
+      knownAddressesCache.getDisplayInfoFor = jest.fn().mockReturnValue({
+        name: 'recipient-name',
+        imageUrl: 'recipient-image-url',
+      })
+      expect(await tokenSent.getEvent(mockTokenSentTx)).toEqual({
+        type: 'SENT',
+        timestamp: 1670456975000,
+        transactionHash: 'correct-hash',
+        block: '0',
+        amount: {
+          value: 1.233468,
+          tokenAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+          timestamp: 1670456975000,
+        },
+        address: 'to-address',
+        account: 'to-address',
+        fees: [], // TODO add fees once wallet can handle fees paid in native currency https://linear.app/valora/issue/ACT-840/display-fees-correctly-when-paid-in-native-token
+        metadata: {
+          title: 'recipient-name',
+          image: 'recipient-image-url',
+        },
+      })
+      expect(knownAddressesCache.getDisplayInfoFor).toHaveBeenCalledWith(
+        'to-address',
+      )
     })
   })
 })
